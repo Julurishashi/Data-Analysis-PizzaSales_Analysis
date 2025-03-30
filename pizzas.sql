753b4b216ffbc6d04a4abc40efd393e6fdd33b2d
Julurishashi <147416545+Julurishashi@users.noreply.github.com>
2025-03-30 15:47:21 +0530

    Add files via upload

---
+++ pizzas.sql
@@ -0,0 +1,133 @@
+use projectmarch;
+
+select * from pizza_sales;
+
+#total revenue
+select round(sum(total_price),2)  as total_revenue from pizza_sales;  #'817860.05'
+
+#average order value  total_revenue by total orders
+
+select sum(total_price) as total_revenue ,count(distinct order_id) as total_orders , (sum(total_price)/count(distinct order_id)) as average_order_value from pizza_sales;
+
+
+#'817860.05', '48620', avg reve  '38.30' 
+
+#pizzas sold 
+select sum(quantity)as   totalpizzas from pizza_sales;  #49574
+
+#total orders 
+select count(distinct order_id ) as totalorders from pizza_sales;  #21350 
+
+#average pizza per order 
+select sum(quantity) as totalsold , count(distinct order_id) as totalorders , round(sum(quantity)/count(distinct order_id),2) as avgpiz from pizza_sales; #2.32 
+
+
+select * from pizza_sales;
+
+-- alter table pizza_sales modify column order_date ,  STR_TO_DATE(order_date,'%Y-%m-%d') ;
+
+update pizza_sales set order_date=str_to_date(order_date,'%d-%m-%Y');
+
+select year(order_date) as y from pizza_sales;	
+
+select * from pizza_sales;
+
+update pizza_sales set order_time=str_to_date(order_time,'%H:%i:%m');
+UPDATE pizza_sales
+SET order_time = STR_TO_DATE(order_time, '%H:%i:%s');
+
+
+select hour(order_time) from pizza_sales;
+
+
+
+
+select dayname(order_date) as order_day , count(distinct order_id ) as total_order from pizza_sales group by dayname(order_date);	
+
+#month 
+
+select monthname(order_date) as month , count(distinct order_id) as total_orders from pizza_sales group by monthname(order_date) order by count(distinct order_id) desc ;
+
+#percentage of sales by pizza cate
+select * from pizza_sales;
+
+with cattot as (
+select pizza_category , sum(total_price)  as cattotal from pizza_sales group by  pizza_category )
+
+select  c.* , (c.cattotal / sum(p.total_price) )* 100 from cattot c join pizza_sales p on c.pizza_category=p.pizza_category group by p.pizza_category; 
+
+select * from pizza_sales;
+
+select pizza_category, sum(total_price) as total_sales ,(sum(total_price)   / (select sum(total_price) from pizza_sales where month(order_date)=1 
+ )  ) * 100   as pct from pizza_sales
+ where month(order_date)=1 
+ group by pizza_category;
+ 
+ 
+ #percentage of sales by pizza size
+ 
+ select * from pizza_sales;
+ 
+ select pizza_size , round(sum(total_price)  * 100  / ( select sum(total_price) from pizza_sales where quarter(order_date) = 1 ) ,2 )  as persale_by_size
+ 
+ from pizza_sales where quarter(order_date) = 1  group by pizza_size order by sum(total_price) desc;
+ 
+ 
+ 
+ 
+ 
+ select sum(total_price) as  total_revenue,sum(quantity) as total_quantity, count(distinct order_id) as  total_order from pizza_sales;
+ 
+ 
+ #
+ 
+ 
+--  select total_price , quantity ,  count( distinct order_id )from pizza_sales  group by  order by total_price , quantity  desc ;
+
+#pizza name , revenue , by quantity , total quantity , orders  by asc  and dsc
+
+
+#by revenue asc bottom 5 
+select  pizza_name , sum(total_price )  as  revenue from pizza_sales group by   pizza_name order by sum(total_price ) asc limit 5 ;
+-- 11588.4999999999
+-- 13955.75
+-- 15277.75
+-- 15360.5
+-- 15596
+
+select pizza_name ,  sum(total_price) as revenue from pizza_sales group by pizza_name order by sum(total_price ) desc limit 5 ;
+
+-- The Thai Chicken Pizza	43434.25
+-- The Barbecue Chicken Pizza	42768
+-- The California Chicken Pizza	41409.5
+-- The Classic Deluxe Pizza	38180.5
+-- The Spicy Italian Pizza	34831.25
+
+
+# by pizza_quantity 
+select  pizza_name , sum(quantity) as quantity from pizza_sales group by pizza_name order by sum(quantity) asc limit 5; 
+-- The Brie Carre Pizza	490
+-- The Mediterranean Pizza	934
+-- The Calabrese Pizza	937
+-- The Spinach Supreme Pizza	950
+-- The Soppressata Pizza	961
+
+
+select pizza_name , sum(quantity) as  quantity from pizza_sales group by pizza_name order by sum(quantity) desc limit 5;
+ -- The Classic Deluxe Pizza	2453
+-- The Barbecue Chicken Pizza	2432
+-- The Hawaiian Pizza	2422
+-- The Pepperoni Pizza	2418
+-- The Thai Chicken Pizza	2371
+
+# by total orders 
+
+select  pizza_name , count(distinct order_id) as total_order from pizza_sales 
+group by pizza_name order by total_order asc limit 5;
+
+
+
+select pizza_name,count(distinct order_id ) as total_ordrs from pizza_sales 
+group by pizza_name order by total_ordrs desc limit 5;
+ 
+ 
